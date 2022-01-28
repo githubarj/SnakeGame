2caf5b5ec9ccc0439ddf2e58c0f12ad6dec85d5e
githubarj <85483196+githubarj@users.noreply.github.com>
2022-01-28 06:34:50 +0300

    Window Color and Projection

--- main.cpp
+++ main.cpp
@@ -2,21 +2,40 @@
 #include <GL/glut.h>
 
 void display_callback();
+void reshape_callback(int,int);
+
+void init()
+{
+    glClearColor(0.0,0.0,0.0,1.0);
+}
 
 int main(int argc,char **argv)
 {
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-    //glutWindowSize(500,500);
+    glutInitWindowSize(500,500);
     glutCreateWindow("SNAKE");
+    glutReshapeFunc(reshape_callback);
     glutDisplayFunc(display_callback);
+    init();
     glutMainLoop();
     return 0;
 }
 
 void display_callback()
 {
+    glClear(GL_COLOR_BUFFER_BIT);
     glutSwapBuffers();
+}
+
+void reshape_callback(int w ,int h)
+{
+    glViewport(0,0,(GLsizei)w,(GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0,40.0,0.0,40.0,-1.0,1.0);
+    glMatrixMode(GL_MODELVIEW);
+
 }
 
 
